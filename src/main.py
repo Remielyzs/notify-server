96f4c648dd0c2f3c53d2e2dd3bfcf54f2fc968d6
Remiel <misakalee@stu.hubu.edu.cn>
2022-05-11 11:35:49 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 
-print(os.getenv('WX_COMPANY_ID'))
+print('WX_COMPANY_ID',os.getenv('WX_COMPANY_ID'))
 print(os.getenv('WX_APP_ID'))
 print(os.getenv('WX_APP_SECRET'))
 print(os.getenv('HEFENG_API_KEY'))
