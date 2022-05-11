d000af76f574c66b733217084eb59c225b052058
Remiel <misakalee@stu.hubu.edu.cn>
2022-05-11 11:45:36 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import os
 
+import yaml
+ 
+file = open('config.yml', 'r', encoding="utf-8")
+#使用文件对象作为参数
+data = yaml.load(file)      
+print(data)
+
 print('WX_COMPANY_ID',os.getenv('WX_COMPANY_ID'))
 print(os.getenv('WX_APP_ID'))
 print(os.getenv('WX_APP_SECRET'))
