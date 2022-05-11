32ecc333c6f22541e7040ea6650018e472a46df6
Remiel <misakalee@stu.hubu.edu.cn>
2022-05-11 18:04:50 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,30 @@
 import os
-
+import requests
 import yaml
- 
-file = open('config.yml', 'r', encoding="utf-8")
-#使用文件对象作为参数
-file_data = file.read()                 
-file.close()
- 
-#指定Loader
-data = yaml.load(file_data,Loader=yaml.FullLoader)    
-print(data)
-print('WX_COMPANY_ID',os.getenv('WX_COMPANY_ID'))
-print(os.getenv('WX_APP_ID'))
-print(os.getenv('WX_APP_SECRET'))
-print(os.getenv('HEFENG_API_KEY'))
+
+def get_config():
+    file = open('config.yml', 'r', encoding="utf-8")
+    #使用文件对象作为参数
+    file_data = file.read()                 
+    file.close()
+    #指定Loader
+    data = yaml.load(file_data,Loader=yaml.FullLoader)    
+    return data
+
+def get_envs():
+    myenvs = {}
+    myenvs['WX_COMPANY_ID'] = os.getenv('WX_COMPANY_ID')
+    myenvs['WX_APP_ID'] = os.getenv('WX_APP_ID')
+    myenvs['WX_APP_SECRET'] = os.getenv('WX_APP_SECRET')
+    myenvs['HEFENG_API_KEY'] = os.getenv('HEFENG_API_KEY')
+    return myenvs
+
+def get_access_token(my):
+    response = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={corpid}&corpsecret={corpsecret}".format(corpid=corpid, corpsecret=corpsecret))
+    data = json.loads(response.text)
+    return data['access_token']
+
+if __name__ == '__main__':
+    config = get_config()
+    envs = get_envs()
+    
